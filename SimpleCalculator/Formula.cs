55fef2059f1c0a8ca80d41a4a4081a327c55a82c
VhonAdrian-Rollan <vhonadrianr@gmail.com>
2020-10-21 13:57:33 +0800

    Fourth Repos

---
+++ SimpleCalculator/Formula.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public delegate int GetAnswer(int num1, int num2);
+    class Formula
+    {
+        public static int getSum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int getDifference(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int getProduct(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int getQuotient(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
